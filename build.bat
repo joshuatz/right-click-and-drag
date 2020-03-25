ac205f693ba28d890419435688be3b51e148a3b9
Joshua Tzucker <joshua.tz@gmail.com>
2020-03-25 14:28:02 -0700

    Renamed script and files

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-SET OUT_EXE_NAME=zoom-click-and-drag.exe
+SET SCRIPT_NAME_NO_EXT=right-click-and-drag
 
 REM Stop and clean out any existing exes
 taskkill /FI "IMAGENAME eq %OUT_EXE_NAME%" /F
@@ -6,4 +6,4 @@ DEL %OUT_EXE_NAME%
 
 REM Compile
 SET AHK_COMPILER="%ProgramFiles%\AutoHotkey\Compiler\Ahk2Exe.exe"
-%AHK_COMPILER% /in "%cd%\zoom-click-and-drag.ahk" /out "%cd%\%OUT_EXE_NAME%" /icon "icon.ico"
+%AHK_COMPILER% /in "%cd%\%SCRIPT_NAME_NO_EXT%.ahk" /out "%cd%\%SCRIPT_NAME_NO_EXT%.exe" /icon "icon.ico"
